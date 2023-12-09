52a89b7f8bc053e1a0de7bf8f3d9e84d79be52a6
Mariola <mariolahullings@gmail.com>
2023-12-09 14:38:54 -0500

    setup the UI

--- ByteCoin-Swift/ViewController.swift
+++ ByteCoin-Swift/ViewController.swift
@@ -7,11 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let coinManager = CoinManager()
+    
+    //determines how many columns are in the picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //determines how many rows the picker will have
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //coinManager.currencyArray[row] prints the name of the currency
+        let selectedCurrency = coinManager.currencyArray[row]
+        //this passes the selected currency to the CoinManager via getCoinPrice() method
+        coinManager.getCoinPrice(for: selectedCurrency)
+        print(coinManager.currencyArray[row])
+    }
+    
 
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currencyPicker.dataSource = self
+        //sets the VC class as the delegate of the currencyPicker
+        currencyPicker.delegate = self
     }
 
 
